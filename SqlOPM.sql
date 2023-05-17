b70d2251358264af2ed6860f218d193d8642afba
PrekshaKatre75 <preksha.katre75@gmail.com>
2023-05-17 15:38:31 +0530

    initial

--- SqlOPM.sql
+++ SqlOPM.sql
@@ -4,12 +4,12 @@ EmpId int primary key,
 Ename varchar(20),
 Email  varchar(20),
 Contact int,
-Mgr int references Mgr(Mgrid),
+Mgr int references PMgr(Mgrid),
 Deptno int references Dept(Deptno),
 Pid int references Project(Pid)
 );
 
-create table Mgr(
+create table PMgr(
 Mgrid int primary key,
 Mname varchar(20),
 Email  varchar(20),
@@ -19,14 +19,39 @@ Deptno int references Dept(Deptno),
 Pid int references Project(Pid)
 );
 
-create table Dept(DeptNo int primary key, Dname varchar (20),Descr varchar(20));
+create table Dept(DeptNo int primary key, Dename varchar (20),Mgr int references PMgr(Mgrid));
 
 
+ /*
+3: StakeHolder
+ Sid - Stakeholders Id  (PK)
+ Sname - Stakeholder's Name
+ Mgr - Alloted Manager   (FK)
+ */
+ create table StakeHolder(Sid int primary key, Sname varchar (20),Descr varchar(20));
+ 
+ /*
+
+4: Delivery Manager
+ Did - Delvery Manager Id (PK)
+ Dname - Delvery Manager Name
+ Mgr - Manger's Id (FK)
+ Location - location of Delvery Manager 
+ Pid - ProjectId (FK)*/
 
 
-
-
-
-
-
+ create table DMgr(Did int primary key, Dname 
+ varchar (20),Location varchar(20),Pid int references Project(Pid),Mgr int references PMgr(Mgrid));
+ 
+ /*
+ Project 
+ Pid - ProjectId (PK)
+ Pname - Project Name
+ Pdsc - Project Description
+ userName - User / Company / Organization's Name
+ status - Handled or Unhandled*/
+ 
+ create table DMgr(Pid int primary key, Pname 
+ varchar (20),Pdsc varchar(20),userName varchar(20),status varchar(20));
+ 
  
